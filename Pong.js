d087bdbfc4deabfd85ee756813c24804acf7d36f
Nitzan <nitzan.orr@gmail.com>
2015-12-05 22:47:42 -0800

    Add proper movement to left bar

--- Pong.js
+++ Pong.js
@@ -8,72 +8,110 @@ ctx.fillStyle = "white";
 
 var reachedTop = false; 
 var reachedBottom = false;
+var leftMoveVal = 0;
 
 //width: 480, height: 320
 ctx.fillRect(10, leftBarYPos, 10, 60);
 ctx.fillRect(460, rightBarYPos, 10, 60);
 
 
-//document.addEventListener('keydown', function(e) {
-//    // 'e' short for 'event'
-//    switch(e.keyCode) {
+document.addEventListener('keydown', function(e) {
+    // 'e' short for 'event'
+    switch(e.keyCode) {
 //        case 32:
 //            pause = !pause;
 //            break;
-//        case 38:
-//        case 87:
-//            tempDirec = -1;
-//            break;
-//        case 40:
-//        case 83:
-//            tempDirec = 1;
-//            break;
+        case 38: //up
+        case 87: //w
+            leftMoveVal = 1;
+            break;
+        case 40: //down
+        case 83: //s
+            leftMoveVal = -1;
+            break;
 //        case 37:
 //        case 65:
-//            tempDirec = 2;
+//            leftMoveVal = 2;
 //            break;
 //        case 39:
 //        case 68:
-//            tempDirec = 4;
-//    }
+//            leftMoveVal = 4;
+        default:
+            leftMoveVal = 0;
+            break;
+    }
 //    if (direcS < 2 && tempDirec > 1 || direcS > 1 && tempDirec < 2) {
 //        direcS = tempDirec;
-//    }
-//}, false);
+//  }
+}, false);
+
+
+document.addEventListener('keyup', function(e) {
+    // 'e' short for 'event'
+    switch(e.keyCode) {
+//        case 32:
+//            pause = !pause;
+//            break;
+        case 38: //up
+        case 87: //w
+            leftMoveVal = 0;
+            break;
+        case 40: //down
+        case 83: //s
+            leftMoveVal = 0;
+            break;
+//        case 37:
+//        case 65:
+//            leftMoveVal = 2;
+//            break;
+//        case 39:
+//        case 68:
+//            leftMoveVal = 4;
+        default:
+            leftMoveVal = 0;
+            break;
+    }
+}, false);
+
+
+
+
+
 
 
 setInterval(function(){
     
     //left bar
-    if(!reachedTop)
+    if(!reachedTop && leftMoveVal == 1)
     {
+        reachedBottom = false;
         ctx.fillStyle = "black";
-        ctx.fillRect(10, leftBarYPos--, 10, 60);
+        ctx.fillRect(10, leftBarYPos, 10, 60);
+        leftBarYPos -= 4;
         ctx.fillStyle = "white";
         ctx.fillRect(10, leftBarYPos, 10, 60);
-        if(leftBarYPos == 0)
+        if(leftBarYPos < 0)
         {
             reachedTop = true;
-            reachedBottom = false;
+            //reachedBottom = false;
         }
     }
     
-    else if(!reachedBottom)
+    else if(!reachedBottom && leftMoveVal == -1)
     {
+        reachedTop = false;
         ctx.fillStyle = "black";
-        ctx.fillRect(10, leftBarYPos++, 10, 60);
+        ctx.fillRect(10, leftBarYPos, 10, 60);
+        leftBarYPos += 4;
         ctx.fillStyle = "white";
         ctx.fillRect(10, leftBarYPos, 10, 60);
-        if(leftBarYPos == canvas.height - 60)
+        if(leftBarYPos > canvas.height - 60)
         {
             reachedBottom = true;
-            reachedTop = false;
+            //reachedTop = false;
         }
     }
     
     
-    
-   
-    
-}, 10);
+}, 30);
    
